fe3123c564f1ec7ef5064314d07e5744e7716da3
emiklil <mikael.lilliestierna@ericsson.com>
2013-05-08 08:21:28 +0200

    added loginscreen

--- src/se/ericsson/com2android/MainNavigationActivity.java
+++ src/se/ericsson/com2android/MainNavigationActivity.java
@@ -6,6 +6,9 @@ import com.example.com2android.R;
 
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -15,6 +18,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainNavigationActivity extends FragmentActivity {
 
@@ -25,54 +30,87 @@ public class MainNavigationActivity extends FragmentActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.fragment_main);
+		//		setContentView(R.layout.fragment_main);
+		setContentView(R.layout.login);
 
-		Fragment tabCliFragment = new CliFragment();
-		Fragment tabNcFragment = new NcFragment();
-		Fragment tabHubFragment = new HubFragment();
+		Typeface tf = Typeface.createFromAsset(getAssets(),"fonts/Hashtag.ttf");
 
-		PagerAdapter pageAdapter = new PagerAdapter(getSupportFragmentManager());
-		pageAdapter.addFragment(tabCliFragment);
-		pageAdapter.addFragment(tabNcFragment);
-		pageAdapter.addFragment(tabHubFragment);
+		LinearLayout ll = (LinearLayout) findViewById(R.id.loginLayout);
 
-		viewPager = (ViewPager) findViewById(R.id.pager);
-		viewPager.setAdapter(pageAdapter);
-		viewPager.setOffscreenPageLimit(2);
-		viewPager.setCurrentItem(0);
-		
-		viewPager.setOnTouchListener(new View.OnTouchListener() {
-			
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-		});
-		
 
-		viewPager.setOnPageChangeListener(
-				new ViewPager.SimpleOnPageChangeListener() {
-					@Override
-					public void onPageSelected(int position) {
-						getActionBar().setSelectedNavigationItem(position);
-					}
-				});
+		TextView tv1 = (TextView) findViewById(R.id.username_text_login);
+		TextView tv2 = (TextView) findViewById(R.id.password_text_login);
+		TextView tv3 = (TextView) findViewById(R.id.cli_checkbox);
+		TextView tv4 = (TextView) findViewById(R.id.nc_checkbox);
+		TextView tv5 = (TextView) findViewById(R.id.terminal_checkbox);
 
+		tv1.setTextSize(50);
+		tv2.setTextSize(50);
+		tv3.setTextSize(50);
+		tv4.setTextSize(50);
+		tv5.setTextSize(50);
+
+		tv1.setTypeface(tf);
+		tv2.setTypeface(tf);
+		tv3.setTypeface(tf);
+		tv4.setTypeface(tf);
+		tv5.setTypeface(tf);
+
+
+		//		Fragment tabCliFragment = new CliFragment();
+		//		Fragment tabNcFragment = new NcFragment();
+		//		Fragment tabHubFragment = new HubFragment();
+		//
+		//		PagerAdapter pageAdapter = new PagerAdapter(getSupportFragmentManager());
+		//		pageAdapter.addFragment(tabCliFragment);
+		//		pageAdapter.addFragment(tabNcFragment);
+		//		pageAdapter.addFragment(tabHubFragment);
+		//
+		//		viewPager = (ViewPager) findViewById(R.id.pager);
+		//		viewPager.setAdapter(pageAdapter);
+		//		viewPager.setOffscreenPageLimit(2);
+		//		viewPager.setCurrentItem(0);
+		//		
+		//		viewPager.setOnTouchListener(new View.OnTouchListener() {
+		//			
+		//			@Override
+		//			public boolean onTouch(View v, MotionEvent event) {
+		//				// TODO Auto-generated method stub
+		//				return false;
+		//			}
+		//		});
+		//		
+		//
+		//		viewPager.setOnPageChangeListener(
+		//				new ViewPager.SimpleOnPageChangeListener() {
+		//					@Override
+		//					public void onPageSelected(int position) {
+		//						getActionBar().setSelectedNavigationItem(position);
+		//					}
+		//				});
+		//
 		ActionBar ab = getActionBar();
 		ab.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		Tab tab1 = ab.newTab().setText("CLI")
-				.setTabListener(new TabListener<CliFragment>(this, "CLI", CliFragment.class));
+		ab.setStackedBackgroundDrawable(new ColorDrawable(Color.RED));
+		//				Tab tab1 = ab.newTab().setText("CLI")
+		//						.setTabListener(new TabListener<CliFragment>(this, "CLI", CliFragment.class));
+		//		
+		//				Tab tab2 = ab.newTab().setText("NC")
+		//						.setTabListener(new TabListener<NcFragment>(this, "NC", NcFragment.class));
+		//		
+		//				Tab tab3 = ab.newTab().setText("HUB")
+		//						.setTabListener(new TabListener<HubFragment>(this, "Hub", HubFragment.class));
+		//		
 
-		Tab tab2 = ab.newTab().setText("NC")
-				.setTabListener(new TabListener<NcFragment>(this, "NC", NcFragment.class));
-
-		Tab tab3 = ab.newTab().setText("HUB")
-				.setTabListener(new TabListener<HubFragment>(this, "Hub", HubFragment.class));
-
-		ab.addTab(tab1);
-		ab.addTab(tab2);
-		ab.addTab(tab3);
+//		Tab tab1 = ab.newTab().setText("CLI");
+//
+//		Tab tab2 = ab.newTab().setText("NC");
+//
+//		Tab tab3 = ab.newTab().setText("HUB");
+//
+//		ab.addTab(tab1);
+//		ab.addTab(tab2);
+//		ab.addTab(tab3);
 	}
 
 
